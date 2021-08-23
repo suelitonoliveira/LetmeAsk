3d06311089c04fa840043e6a7a7464aa46a70bb3
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-08-23 17:53:05 -0300

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,39 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleImage from '../assets/images/google-icon.svg';
+
+import '../styles/auth.scss';
+import { Button } from '../components/Button';
+
+export function Home(){
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt="Illustação simbolizando perguntas e respostas" />
+                <strong>Crie salas de Q&amp;A  ao-vivo</strong>
+                <p>Tire as dúvidas da sua audiência em tempo-real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                <img src={logoImg} alt="letmeask" />
+      
+            <button className="create-room">
+                <img src={googleImage} alt="Logo do Google" />
+                Crie sua sala com o google
+            </button>
+
+            <div className="separator">ou entre em uma sala</div>
+                <form>
+                <input 
+                type="text" 
+                placeholder="Digite o código da sala"
+                />
+                <Button type="submit"> 
+                    Entrar na sala
+                </Button>
+                </form>
+               </div>
+            </main>
+        </div>
+    )
+}

b8d2211ecd4b6173545b1dfe4726cb3cb03db03d
suelitonoliveira <suelitondeoliveira@gmail.com>
2021-08-24 18:22:39 -0300

    implementando autenticação google

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,76 @@
+import firebase from "firebase";
+import { useEffect } from "react";
+import { useState } from "react";
+import { createContext, ReactNode } from "react";
+import { auth } from "../service/firebase";
+
+
+type User = {
+    id: string;
+    name: string;
+    avatarURL: string;
+  }
+  
+  type AuthContext = {
+    user: User | undefined;
+    sigInWithGoogle: () => Promise<void>;
+  }
+  type AuthContextProviderProps = {
+      children: ReactNode;
+  }
+
+export const AuthContext = createContext({} as AuthContext);
+
+export function AuthContextProvider(props: AuthContextProviderProps){
+
+    const [user, setUser] = useState<User>();
+
+    useEffect(() => {
+   const unsubscribe = auth.onAuthStateChanged(user =>{
+        if (user){
+          const { displayName, photoURL, uid } = user;
+  
+          if (!displayName || !photoURL) {
+            throw new Error("Missing information from Google Account.");
+          }
+          setUser({
+            id: uid,
+            name: displayName,
+            avatarURL: photoURL
+          })
+  
+        }
+      })
+  
+      return () => {
+        unsubscribe();
+      }
+    }, [])
+  
+   async function sigInWithGoogle(){
+      const provider = new firebase.auth.GoogleAuthProvider();
+  
+      const result = await auth.signInWithPopup(provider);
+  
+          if (result.user){
+            const { displayName, photoURL, uid } = result.user;
+            if (!displayName || !photoURL) {
+              throw new Error("Missing information from Google Account.");
+            }
+            setUser({
+              id: uid,
+              name: displayName,
+              avatarURL: photoURL
+            })
+          }
+  
+      }
+
+
+    return(
+
+        <AuthContext.Provider value={{ user , sigInWithGoogle}}>
+            {props.children}
+        </AuthContext.Provider>
+    );
+}
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,29 +1,17 @@
-import {useHistory} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleImage from '../assets/images/google-icon.svg';
 
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
+import { useContext } from 'react';
+//import { useAuth } from '../hooks/useAuth';
 
-import { useAuth } from '../hooks/useAuth';
 
+export function NewRoom(){
 
-
-export function Home(){
-
-    const history = useHistory();
-    const { user, sigInWithGoogle } = useAuth();
-
-   async function handleCreateRoom(){
-        if(!user){
-         await sigInWithGoogle()
-        }
-
-            history.push('/rooms/new');
-
-    }
+    //const { user } = useAuth();
 
     return (
         <div id="page-auth">
@@ -35,22 +23,19 @@ export function Home(){
             <main>
                 <div className="main-content">
                 <img src={logoImg} alt="letmeask" />
-      
-            <button onClick={handleCreateRoom} className="create-room">
-                <img src={googleImage} alt="Logo do Google" />
-                Crie sua sala com o google
-            </button>
-
-            <div className="separator">ou entre em uma sala</div>
+                <h2>Criar uma nova sala</h2>
                 <form>
                 <input 
                 type="text" 
-                placeholder="Digite o código da sala"
+                placeholder="Nome da sala"
                 />
                 <Button type="submit"> 
-                    Entrar na sala
+                    Criar na sala
                 </Button>
                 </form>
+                <p>
+                    Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
+                </p>
                </div>
             </main>
         </div>
